2582c3296ea0fb79b5bce840e066ba7b01c5b24b
elbasel-404 <abdelrahman.elbasel42@gmail.com>
2026-04-06 15:58:22 +0200

    ...

--- app/getData.ts
+++ app/getData.ts
@@ -3,12 +3,13 @@
 export const getData = async (index: number) => {
   const randomNum = Math.floor(Math.random() * 10);
 
-  const prom = new Promise<string>((resolve) => {
-    setTimeout(() => {
-      resolve(`index: ${index}, random number: ${randomNum}`);
-    }, 1000);
-  });
+  return `index: ${index}, random number: ${randomNum}`;
+  //   const prom = new Promise<string>((resolve) => {
+  //     setTimeout(() => {
+  //       resolve(`index: ${index}, random number: ${randomNum}`);
+  //     }, 1000);
+  //   });
 
-  const data: string = await prom;
-  return data;
+  //   const data: string = await prom;
+  //   return data;
 };
