ed464a53487d08a292a34508037caa5f7e56ba19
elbasel-404 <abdelrahman.elbasel42@gmail.com>
2026-04-06 15:46:26 +0200

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { getData } from "./getData";
 
-export default async function Home() {
-  const data = await getData();
+export default function Home() {
+  const [data, setData] = useState("Loading");
+
+  const fetchData = async () => {
+    const result = await getData();
+    setData(result);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetchData();
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div>
       <p>{data}</p>
